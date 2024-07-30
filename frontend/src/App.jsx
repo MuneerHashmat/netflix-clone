ed1200531c37147f5b5f4fb9a3d2ec90b1a91e23
Muneer Hashmat <muneerhashmat@gmail.com>
2024-07-30 13:50:13 +0530

    built landing page, login page and signup page in the frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,17 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+
 function App() {
   return (
-    <>
-      <div className="text-2xl text-center mt-10">
-        <h1>Hello World</h1>
-      </div>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </Router>
   );
 }
 
