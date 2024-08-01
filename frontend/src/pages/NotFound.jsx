74a7994c4dc68acf48be1796f566819d6c94b3b1
Muneer Hashmat <muneerhashmat@gmail.com>
2024-08-01 17:17:47 +0530

    fixed minor bugs

--- frontend/src/pages/NotFound.jsx
+++ frontend/src/pages/NotFound.jsx
@@ -3,10 +3,10 @@ import { Link } from "react-router-dom";
 const NotFound = () => {
   return (
     <div
-      className="min-h-screen bg-cover bg-center flex flex-col justify-center items-center text-white"
+      className="w-screen h-screen bg-cover bg-center flex flex-col justify-center items-center text-white overflow-hidden"
       style={{ backgroundImage: `url('/404.png')` }}
     >
-      <header className="absolute top-0 left-0 p-4 bg-black w-full ">
+      <header className="absolute top-0 left-0 p-4 bg-black w-full">
         <Link to={"/"}>
           <img src="/netflix-logo.png" alt="Netflix" className="h-8" />
         </Link>
